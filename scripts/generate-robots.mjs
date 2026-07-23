e6c0598d7fbff622717d6069087421addcbc81c8
balikrotasi1041 <balikrotasi1041@gmail.com>
2026-07-24 00:21:22 +0300

    Add files via upload

---
+++ scripts/generate-robots.mjs
@@ -0,0 +1,12 @@
+import { writeFileSync, readFileSync } from "node:fs";
+const site=(process.env.PUBLIC_SITE_URL||"https://oltaatlasi.com").replace(/\/$/,"");
+writeFileSync("public/robots.txt",`User-agent: *\nAllow: /\n\nSitemap: ${site}/sitemap-index.xml\nSitemap: ${site}/sitemap-images.xml\n`);
+const source=readFileSync("src/data/meralar.ts","utf8");
+const match=source.match(/export const meralar: Mera\[\] = (\[.*?\n\]);\n\nexport const provinces/s);
+if(!match) throw new Error("Mera verisi image sitemap için okunamadı.");
+const meralar=JSON.parse(match[1]);
+const escapeXml=(value)=>String(value).replaceAll("&","&amp;").replaceAll("<","&lt;").replaceAll(">","&gt;").replaceAll('"',"&quot;").replaceAll("'","&apos;");
+const entries=meralar.map((m)=>`  <url>\n    <loc>${site}/meralar/${escapeXml(m.slug)}/</loc>\n    <image:image>\n      <image:loc>${site}${escapeXml(m.socialImage)}</image:loc>\n      <image:title>${escapeXml(m.name)} balık avı rotası</image:title>\n      <image:caption>${escapeXml(m.summary)}</image:caption>\n    </image:image>\n  </url>`).join("\n");
+const xml=`<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n${entries}\n</urlset>\n`;
+writeFileSync("public/sitemap-images.xml",xml);
+console.log(`robots.txt ve ${meralar.length} görselli sitemap hazır: ${site}`);
