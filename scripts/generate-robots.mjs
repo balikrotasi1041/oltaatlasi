d81692c99cc1acef40473b5d5ade22dbcdf699fd
balikrotasi1041 <balikrotasi1041@gmail.com>
2026-07-27 20:52:34 +0300

    Include daily locations in image sitemap generation

--- scripts/generate-robots.mjs
+++ scripts/generate-robots.mjs
@@ -1,10 +1,13 @@
 import { writeFileSync, readFileSync } from "node:fs";
 const site=(process.env.PUBLIC_SITE_URL||"https://oltaatlasi.com").replace(/\/$/,"");
 writeFileSync("public/robots.txt",`User-agent: *\nAllow: /\n\nSitemap: ${site}/sitemap-index.xml\nSitemap: ${site}/sitemap-images.xml\n`);
-const source=readFileSync("src/data/meralar.ts","utf8");
-const match=source.match(/export const meralar: Mera\[\] = (\[.*?\n\]);\n\nexport const provinces/s);
-if(!match) throw new Error("Mera verisi image sitemap için okunamadı.");
-const meralar=JSON.parse(match[1]);
+const baseSource=readFileSync("src/data/meralar.ts","utf8");
+const baseMatch=baseSource.match(/export const meralar: Mera\[\] = (\[.*?\n\]);\n\nexport const provinces/s);
+if(!baseMatch) throw new Error("Temel mera verisi image sitemap için okunamadı.");
+const dailySource=readFileSync("src/data/meralar-gunluk.ts","utf8");
+const dailyMatch=dailySource.match(/export const gunlukMeralar: Mera\[\] = (\[.*\]);\s*$/s);
+if(!dailyMatch) throw new Error("Günlük mera verisi image sitemap için okunamadı.");
+const meralar=[...JSON.parse(baseMatch[1]),...Function(`"use strict"; return (${dailyMatch[1]});`)()];
 const escapeXml=(value)=>String(value).replaceAll("&","&amp;").replaceAll("<","&lt;").replaceAll(">","&gt;").replaceAll('"',"&quot;").replaceAll("'","&apos;");
 const entries=meralar.map((m)=>`  <url>\n    <loc>${site}/meralar/${escapeXml(m.slug)}/</loc>\n    <image:image>\n      <image:loc>${site}${escapeXml(m.socialImage)}</image:loc>\n      <image:title>${escapeXml(m.name)} balık avı rotası</image:title>\n      <image:caption>${escapeXml(m.summary)}</image:caption>\n    </image:image>\n  </url>`).join("\n");
 const xml=`<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n${entries}\n</urlset>\n`;
