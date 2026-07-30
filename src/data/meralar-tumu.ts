77b7f681fa4925565c5cf0b441fbad28db3dde32
balikrotasi1041 <balikrotasi1041@gmail.com>
2026-07-30 20:16:27 +0300

    feat: include 2026-07-30 verified route

--- src/data/meralar-tumu.ts
+++ src/data/meralar-tumu.ts
@@ -1,7 +1,8 @@
 import { meralar as temelMeralar } from "./meralar";
 import { gunlukMeralar } from "./meralar-gunluk";
+import { meralar20260730 } from "./meralar-2026-07-30";
 
-export const meralar = [...temelMeralar, ...gunlukMeralar];
+export const meralar = [...temelMeralar, ...gunlukMeralar, ...meralar20260730];
 export const provinces = [...new Set(meralar.map((mera) => mera.province))].sort((a,b)=>a.localeCompare(b,"tr"));
 export const districtsByProvince = Object.fromEntries(provinces.map((province) => [province, [...new Set(meralar.filter((m)=>m.province===province).map((m)=>m.district))].sort((a,b)=>a.localeCompare(b,"tr"))]));
 export const fishOptions = [...new Set(meralar.flatMap((mera) => mera.fish))].sort((a,b)=>a.localeCompare(b,"tr"));
