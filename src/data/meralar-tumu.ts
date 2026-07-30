8def0c574e8ac7cb61e446961811b95fa55ac01d
balikrotasi1041 <balikrotasi1041@gmail.com>
2026-07-30 20:37:56 +0300

    feat: include national routes in site data

--- src/data/meralar-tumu.ts
+++ src/data/meralar-tumu.ts
@@ -1,7 +1,8 @@
 import { meralar as temelMeralar } from "./meralar";
 import { gunlukMeralar } from "./meralar-gunluk";
+import { ulusalMeralar } from "./meralar-ulusal";
 
-export const meralar = [...temelMeralar, ...gunlukMeralar];
+export const meralar = [...temelMeralar, ...gunlukMeralar, ...ulusalMeralar];
 export const provinces = [...new Set(meralar.map((mera) => mera.province))].sort((a,b)=>a.localeCompare(b,"tr"));
 export const districtsByProvince = Object.fromEntries(provinces.map((province) => [province, [...new Set(meralar.filter((m)=>m.province===province).map((m)=>m.district))].sort((a,b)=>a.localeCompare(b,"tr"))]));
 export const fishOptions = [...new Set(meralar.flatMap((mera) => mera.fish))].sort((a,b)=>a.localeCompare(b,"tr"));
