3f660f7bdc6d0ec3585144eff6421f13cda7dc02
balikrotasi1041 <balikrotasi1041@gmail.com>
2026-07-30 23:36:31 +0300

    data: add generated national research cache

---
+++ src/data/meralar-ulusal-otomatik-arastirma.js
@@ -0,0 +1,10 @@
+export const ulusalOtomatikArastirmaMeta = {
+  generatedAt:null,
+  routeCount:0,
+  coordinateRouteCount:0,
+  fishEvidenceRouteCount:0,
+  accessEvidenceRouteCount:0,
+  providerNotes:[]
+};
+
+export const ulusalOtomatikArastirma = {};
